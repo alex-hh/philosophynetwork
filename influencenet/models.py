34efb9b77d7a18d4da16cf435a7131e89f516dfc
alex-hh <alex.hhooker@gmail.com>
2016-04-09 19:36:04 +0100

    change influence edge str method

--- influencenet/models.py
+++ influencenet/models.py
@@ -2,20 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Philosopher(models.Model):
-    name=models.CharField(max_length=200)
-    date_of_birth=models.DateTimeField(null=True)
-    influences=models.ManyToManyField("self", related_name='follower')
-    freebase_id=models.CharField(max_length=50,null=True)
-    dbpedia_link=models.CharField(max_length=200,null=True)
+    name = models.CharField(max_length=200)
+    date_of_birth = models.DateTimeField(null=True)
+    influences = models.ManyToManyField("self", related_name='follower')
+    freebase_id = models.CharField(max_length=50,null=True)
+    dbpedia_link = models.CharField(max_length=200,null=True)
 
 class InfluenceNode(models.Model):
-    name=models.CharField(max_length=200)
-    date_of_birth=models.DateTimeField(null=True)
-    freebase_id=models.CharField(max_length=50,null=True)
-    dbpedia_link=models.CharField(max_length=200,null=True)
-    is_philosopher=models.BooleanField(default=False)
-    is_writer=models.BooleanField(default=False)
+    name = models.CharField(max_length=200)
+    date_of_birth = models.DateTimeField(null=True)
+    freebase_id = models.CharField(max_length=50,null=True)
+    dbpedia_link = models.CharField(max_length=200,null=True)
+    is_philosopher = models.BooleanField(default=False)
+    is_writer = models.BooleanField(default=False)
 
 class InfluenceEdge(models.Model):
-    influencer=models.ForeignKey(InfluenceNode, related_name='influencer')
-    follower=models.ForeignKey(InfluenceNode, related_name='follower')
+    influencer = models.ForeignKey(InfluenceNode, related_name='influencer')
+    follower = models.ForeignKey(InfluenceNode, related_name='follower')
+
+    def __str__(self):
+        return self.influencer.name.split(" ")[-1] + " -> " + self.follower.name.split(" ")[-1]
